300e0cddbfb52cdfc56b20e15148def0044e5bf5
Naim Giger <naim_giger@sluz.ch>
2023-05-25 14:05:26 +0200

    B4

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 class TweetController extends Controller
 {
     public function index() {
-        return ['data' => Tweet::all()];
+        $tweets = Tweet::all();
+        $tweets = $tweets->map(function ($tweet) {
+            $tweet->user = [
+                'name' => 'Franzi Musterfrau'
+            ];
+            return $tweet;
+        });
+        return ['data' => $tweets];
     }
 }
