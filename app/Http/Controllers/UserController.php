384f47069ecf49a4415fe55a664bd6d80403b498
Naim Giger <naim_giger@sluz.ch>
2023-05-30 09:34:09 +0200

    E3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -9,6 +10,6 @@ class UserController extends Controller
 {
     public function show(int $id)
     {
-        return ['data' => User::find($id)->first()];
+        return UserResource::make(User::find($id));
     }
 }
