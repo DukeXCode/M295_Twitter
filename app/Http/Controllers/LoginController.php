a4ce18960348606f0ed2647431d5327ede94bc64
Naim Giger <naim_giger@sluz.ch>
2023-05-30 11:39:33 +0200

    F1, F2, F3

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            return ['token' => $request->user()->createToken('auth-token')->plainTextToken];
+        }
+        return response()->json(['errors' => ['general' => 'E-Mail oder Passwort falsch.']], 422);
+    }
+}
