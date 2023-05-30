ed87c0534343f79d5db9d551cdb28e2e0c9d71cd
Naim Giger <naim_giger@sluz.ch>
2023-05-30 09:31:56 +0200

    E2

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -4,13 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\TweetResource;
 use App\Models\Tweet;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class TweetController extends Controller
 {
     public function index() {
-        $tweets = Tweet::latest()->take(100)->get();
+        $tweets = Tweet::with('user')->latest()->take(100)->get();
         return TweetResource::collection($tweets);
     }
 }
